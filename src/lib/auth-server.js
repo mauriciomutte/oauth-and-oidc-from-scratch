25d8541fac33174b76162ac1be259d325efbae38
Mauricio Mutte <mauriciomutte.mm@gmail.com>
2024-02-21 20:20:19 -0300

    feat: add `clients` and `auth-server` data in lib folder

---
+++ src/lib/auth-server.js
@@ -0,0 +1,8 @@
+// authorization server information
+export const authServer = {
+  authorizationEndpoint: 'http://localhost:9001/authorize',
+  tokenEndpoint: 'http://localhost:9001/token',
+  revocationEndpoint: 'http://localhost:9001/revoke',
+  registrationEndpoint: 'http://localhost:9001/register',
+  userInfoEndpoint: 'http://localhost:9001/userinfo',
+}
---
+++ src/lib/clients.js
@@ -0,0 +1,10 @@
+// client information
+export const client = {
+  client_id: 'oauth-client-1',
+  client_secret: 'oauth-client-secret-1',
+  redirect_uris: ['http://localhost:9000/callback'],
+  scope: 'foobar',
+}
+
+// array with all clients
+export const allClients = [client]
